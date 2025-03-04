6e282ebf5f36f768f6cf569d3097f0acb461674c
Chifrica <chikaonwunali2019@gmail.com>
2025-03-04 10:27:45 +0100

    Initialize Expo project with basic structure and configuration files

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+// import OnboardingScreen from "./(root)/OnboardingScreen";
+
+// export default function index() {
+  
+//   return (
+//     <SafeAreaView>
+//       <OnboardingScreen />
+//     </SafeAreaView>
+//   );
+// }
