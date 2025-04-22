5d7722001d92cbddc7e6bfe8516cb02c42c953e9
Cole Crown <aderibigbeadedamolajohn@gmail.com>
2025-04-22 09:58:33 +0100

    profile type removed

--- types/index.ts
+++ types/index.ts
@@ -3,14 +3,3 @@ export interface DateTimeSelection {
 	formattedDate: string;
 	time: string;
 }
-
-export interface UserData {
-	fullName?: string;
-	email?: string;
-	phoneNumber?: string;
-	gender?: string;
-	dateOfBirth?: string;
-	id?: string;
-	name?: string;
-	profilePicture?: string;
-}
